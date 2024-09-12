286e055acb3ab662f4ef2165d3656f405bcff643
ahka07001 <ahka07001@E-PW05CGQT>
2024-09-12 14:32:47 +0200

    Add project files.

---
+++ InlämningsUppgift1.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace inlämningsuppgift_1._2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("DIN DATOR HAR KRASCHAT");
+        }
+    }
+}
+   
